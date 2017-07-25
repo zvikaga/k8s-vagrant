59fc967eec92f16255eaad904bdbf8dd570ecc98
Zvika Gazit <zvikaga@gmail.com>
2017-07-25 12:30:59 +0300

    vagrant-init fix

--- k8s-vagrant-init.sh
+++ k8s-vagrant-init.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+sudo apt-get update -y
 sudo apt-get install -y virtualbox vagrant nfs-server
-mkdir $HOME/k8s-vagrant && cd $HOME/k8s-vagrant
-if [ ! -f Vagrantfile ]; then vagrant init fi
-mv Vagrantfile.orig
-git clone https://github.com/zvikaga/k8s-vagrant
-vagrant up
+if [ ! -f Vagrantfile ]; then 
+  mkdir $HOME/k8s-vagrant && cd $HOME/k8s-vagrant
+  vagrant init
+  mv Vagrantfile Vagrantfile.orig 
+  git clone https://github.com/zvikaga/k8s-vagrant
+  vagrant up
+EOJ
+else
+  echo "Vagrant already was initialized in this direcotry"
+fi
